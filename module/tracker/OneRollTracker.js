18e9dd4ab9d886f179ab26bc6e687902aa842878
Chris Clouser <waryjack@gmail.com>
2022-08-28 22:03:16 -0400

    tracker app started

--- module/tracker/OneRollTracker.js
+++ module/tracker/OneRollTracker.js
@@ -1,3 +1,28 @@
 export class OneRollTrackWindow extends Application {
 
+    get template() {
+        const path = 'systems/ore/templates/actor/';
+        return `${path}${this.actor.type}sheet.hbs`;
+    }
+
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ['ore', 'sheet', 'app', 'window-app'],
+            width: 775,
+            height: 685,
+            left:120,
+            tabs: [{navSelector: ".tracker-tabs", contentSelector: ".trackerbody", initial: "trackmain"}],
+            dragDrop: [{dragSelector: ".dragline", dropSelector: null}]
+            });
+    }
+
+    /**
+     * @override
+     */
+
+    getData() {
+    
+    }
+
+
 }
