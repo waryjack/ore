ed80549e29b9eaff1f5a4ce4674dcfdc13961dda
waryjack <waryjack@gmail.com>
2025-01-22 17:26:42 -0500

    more sheet experiments, new hit location visual

--- module/templates.js
+++ module/templates.js
@@ -7,6 +7,7 @@ export const preloadHandlebarsTemplates = async function() {
       "systems/ore/templates/partials/armor.hbs",
       "systems/ore/templates/partials/equipment.hbs",
       "systems/ore/templates/partials/hitlocs.hbs",
+      "systems/ore/templates/partials/hitlocs-h.hbs",
       "systems/ore/templates/partials/pools.hbs",
       "systems/ore/templates/partials/powers.hbs",
       "systems/ore/templates/partials/qualities.hbs",
